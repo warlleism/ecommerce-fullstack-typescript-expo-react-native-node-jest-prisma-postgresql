05b1b86c5bfb59f8851c750531e7c4518699bd46
Warllei <warllei.s.martins@hotmail.com>
2025-01-07 03:36:47 -0300

    correções

--- back-end/prisma/migrations/20250101170820_criando/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `logo` on the `Rapide` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Rapide" DROP COLUMN "logo";
